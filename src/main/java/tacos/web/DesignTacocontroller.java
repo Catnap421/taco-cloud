de6c8143e35501eb67081bc764b50f6aa255abc4
catnap421 <wlq7410@naver.com>
2021-01-13 13:09:41 +0900

    Add Post Mapping In Design Route

--- src/main/java/tacos/web/DesignTacocontroller.java
+++ src/main/java/tacos/web/DesignTacocontroller.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.extern.slf4j.Slf4j;
 import tacos.Taco;
@@ -50,6 +51,13 @@ public class DesignTacocontroller {
                         .stream()
                         .filter(x -> x.getType().equals(type))
                         .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public String processDesign(Taco design) {
+        log.info("Processing Design: " + design);
+
+        return "redirect:/orders/current";
     }
 
 }
