335a4bdefa4db0331a5ec1b1ee4aa27b43bc0cec
catnap421 <wlq7410@naver.com>
2021-01-17 13:37:46 +0900

    기본적인 CRUD API 생성

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -3,16 +3,15 @@ package tacos.web;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import lombok.extern.slf4j.Slf4j;
 import tacos.Taco;
@@ -28,8 +27,6 @@ import org.springframework.validation.Errors;
 import tacos.data.TacoRepository;
 import tacos.data.UserRepository;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
@@ -106,6 +103,22 @@ public class DesignTacoController {
         PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
         log.info("최근 저장된 타코 불러오는 중...");
         return tacoRepo.findAll(page).getContent();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+        Optional<Taco> optTaco = tacoRepo.findById(id);
+        log.info("path 변수 사용해서 id에 맞는 타코 가져오기");
+        if (optTaco.isPresent()) {
+            return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepo.save(taco);
     }
 
 }
--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,12 +1,10 @@
 package tacos.web;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
@@ -58,5 +56,33 @@ public class OrderController {
         sessionStatus.setComplete();
 
         return "redirect:/";
+    }
+
+    @PutMapping("/{orderId}")
+    public Order putOrder(@RequestBody Order order) {
+        return orderRepo.save(order);
+    }
+
+    @PatchMapping(path="/{orderId}", consumes="application/json")
+    public Order patchOrder(@PathVariable("orderId") Long orderId, @RequestBody Order patch) {
+        Order order = orderRepo.findById(orderId).get();
+        if (patch.getDeliveryName() != null) order.setDeliveryName(patch.getDeliveryName());
+        if (patch.getDeliveryStreet() != null) order.setDeliveryStreet(patch.getDeliveryStreet());
+        if (patch.getDeliveryCity() != null) order.setDeliveryCity(patch.getDeliveryCity());
+        if (patch.getDeliveryState() != null) order.setDeliveryState(patch.getDeliveryState());
+        if (patch.getDeliveryZip() != null) order.setDeliveryZip(patch.getDeliveryZip());
+        if (patch.getCcNumber() != null) order.setCcNumber(patch.getCcNumber());
+        if (patch.getCcExpiration() != null) order.setCcExpiration(patch.getCcExpiration());
+        if (patch.getCcCVV() != null) order.setCcCVV(patch.getCcCVV());
+
+        return orderRepo.save(order);
+    }
+
+    @DeleteMapping("/{orderId}")
+    @ResponseStatus(code=HttpStatus.NO_CONTENT)
+    public void deleteOrder(@PathVariable("orderId") Long orderId) {
+        try {
+            orderRepo.deleteById(orderId);
+        } catch (EmptyResultDataAccessException e) {}
     }
 }
